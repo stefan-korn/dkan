4b0b70621db99f837c008f0c9a70b133971cd9f7
Stefan Korn <drupal@stefan-korn.de>
2026-03-09 18:20:50 +0100

    identifier of referenced schema properties in search API fields - Github Issue #4310 and custom modifications

--- modules/metastore/modules/metastore_search/src/ComplexData/Dataset.php
+++ modules/metastore/modules/metastore_search/src/ComplexData/Dataset.php
@@ -36,6 +36,15 @@ class Dataset extends ComplexDataFacade {
       $type = $object->properties->{$property}->type ?? "string";
       $defs = self::getPropertyDefinition($type, $object, $property);
       $definitions = array_merge($definitions, $defs);
+      if (in_array($property, $schemaRetriever->getAllIds())) {
+        $property_schema = json_decode($schemaRetriever->retrieve($property));
+        $defs = self::getPropertyDefinition($type, $object, '_Ref_'.$property.'__identifier');
+        $label = isset($property_schema->title) ? $property_schema->title . ' - Identifier' : 'Identifier';
+        $description = $property_schema->description ?? '';
+        $defs['_Ref_'.$property.'__identifier']->setLabel($label);
+        $defs['_Ref_'.$property.'__identifier']->setDescription($description);
+        $definitions = array_merge($definitions, $defs);
+      }
     }
 
     return $definitions;
@@ -48,7 +57,7 @@ class Dataset extends ComplexDataFacade {
     $defs = [];
     if (($type == "array" && isset($object->properties->{$property_name}->items->properties))
     || $type == "object") {
-      $defs = self::getComplexPropertyDefinition($object->properties->{$property_name}, $type, $property_name);
+      $defs = self::getComplexPropertyDefinition($object->properties->{$property_name} ?? FALSE, $type, $property_name);
     }
     else {
       $defs[$property_name] = self::getDefinitionObject($type);
@@ -69,6 +78,9 @@ class Dataset extends ComplexDataFacade {
       $prefix = $property_name . '__item__';
       $props = $property_items->items->properties;
       $child_properties = array_keys((array) $props);
+      $definitions[$property_name] = ListDataDefinition::create('integer');
+      $definitions[$property_name]->setLabel($property_items->title);
+      $definitions[$property_name]->setDescription($property_items->description ?? '');
     }
     elseif ($type == "object" && isset($property_items->properties)) {
       $prefix = $property_name . '__';
@@ -82,7 +94,7 @@ class Dataset extends ComplexDataFacade {
     foreach ($child_properties as $child) {
       $definitions[$prefix . $child] = self::getDefinitionObject($type);
       $definitions[$prefix . $child]->setLabel($props->{$child}->title ?? $property_name);
-      $definitions[$prefix . $child]->setDescription($props->{$child}->description ?? '');
+      $definitions[$prefix . $child]->setDescription($props->{$child}->description ?? $property_name);
     }
     return $definitions;
   }
@@ -124,12 +136,15 @@ class Dataset extends ComplexDataFacade {
 
     if ($definition instanceof ListDataDefinition) {
       $property = new ItemList($definition, $property_name);
-      $values = $this->getArrayValues($property_name);
+      $values = $this->getArrayValues(str_replace('_Ref_', '%Ref:', $property_name));
+      if ($definition->getItemDefinition()->getDataType() === 'integer') {
+        $values = array_keys($values);
+      }
       $property->setValue($values);
     }
     else {
       $property = new StringData($definition, $property_name);
-      $value = $this->getPropertyValue($property_name);
+      $value = $this->getPropertyValue(str_replace('_Ref_', '%Ref:', $property_name));
       $property->setValue($value);
     }
 
@@ -148,8 +163,19 @@ class Dataset extends ComplexDataFacade {
       if (isset($matches[1])
       && isset($this->data->{$matches[1]})
       && isset($matches[2])
-      && isset($this->data->{$matches[1]}->{$matches[2]})) {
-        $value = $this->data->{$matches[1]}->{$matches[2]};
+      && (isset($this->data->{$matches[1]}->{$matches[2]})
+        || is_array($this->data->{$matches[1]}))
+      ) {
+        if (isset($this->data->{$matches[1]}->{$matches[2]})) {
+          $value = $this->data->{$matches[1]}->{$matches[2]};
+        }
+        else {
+          foreach ($this->data->{$matches[1]} as $match) {
+            if (isset($match->{$matches[2]})) {
+              $value[] = $match->{$matches[2]};
+            }
+          }
+        }
       }
     }
     elseif (isset($this->data->{$property_name})) {
@@ -173,8 +199,18 @@ class Dataset extends ComplexDataFacade {
       }
     }
     elseif (isset($this->data->{$property_name})) {
-      $values = $this->data->{$property_name};
+      $values = $this->getPropertyValue($property_name);
       $values = is_string($values) ? json_decode($values) : $values;
+    }
+    elseif (preg_match('/(.*)__(.*)/', (string)$property_name, $sub_checker)
+      && isset($this->data->{$sub_checker[1]})
+      && is_array($this->data->{$sub_checker[1]})
+      && isset($sub_checker[2])
+    ) {
+      $sub_item = reset($this->data->{$sub_checker[1]});
+      if (array_key_exists($sub_checker[2], (array)$sub_item)) {
+        $values = $this->getPropertyValue($property_name);
+      }
     }
 
     return $values;
